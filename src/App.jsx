7b6e3181b43d05057191fd29447001e033df3ab4
MOHD Zeeshan Quraishi <zq8281@gmail.com>
2025-11-28 01:42:04 +0530

    Loading page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import auth from './appwrite/appwite'
+import { login, logout } from './store/slices'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import { Footer, Header } from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
-  return (
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    auth.getCurrentUser()
+      .then((userData)=>{
+        if(userData){
+          dispatch(login({userData}));
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(()=> setLoading(false))
+    }, [])
+
+  return loading? (
     <>
-      <div>Is app working</div>
+      <div>
+        <Header/>
+        pata nhi kya hai
+        <Footer/>
+      </div>
     </>
-  )
+  ): null
 }
 
 export default App
